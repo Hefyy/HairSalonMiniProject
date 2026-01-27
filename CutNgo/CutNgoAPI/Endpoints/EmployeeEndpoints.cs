31e280f11e523bf17668efa95adb309a29481bd1
Hefy's skole PC <henr5c84@zbc.dk>
2026-01-27 09:40:38 +0100

    added api and mock endpoints (minimal api)

---
+++ CutNgo/CutNgoAPI/Endpoints/EmployeeEndpoints.cs
@@ -0,0 +1,13 @@
+﻿namespace CutNgoAPI.Endpoints;
+
+public static class EmployeeEndpoints
+{
+    public static void MapEmployeeEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/employees", () => "Get all employees");
+        app.MapGet("/employees/{id}", (int id) => $"Get employee with ID {id}");
+        app.MapPost("/employees", () => "Create a new employee");
+        app.MapPut("/employees/{id}", (int id) => $"Update employee with ID {id}");
+        app.MapDelete("/employees/{id}", (int id) => $"Delete employee with ID {id}");
+    }
+}
---
+++ CutNgo/CutNgoAPI/Program.cs
@@ -0,0 +1,27 @@
+using Scalar.AspNetCore;
+using CutNgoAPI.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+//Endpoints
+app.MapCustomerEndpoints();
+app.MapEmployeeEndpoints();
+
+
+app.Run();
+
