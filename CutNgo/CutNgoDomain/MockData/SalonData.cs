6b78b8479f019aafa09d5c2252b4cccaad2091ef
epicgaming11 <christina.soerensen0@gmail.com>
2026-03-18 12:39:52 +0100

    Treatment mockdata update

--- CutNgo/CutNgoDomain/MockData/SalonData.cs
+++ CutNgo/CutNgoDomain/MockData/SalonData.cs
@@ -23,25 +23,25 @@ public static class SalonData
     public static List<Treatment> Treatments { get; } = new()
     {
         
-        new Treatment() { Id = 1, Klipning = "Herrer klip", Duration = TimeSpan.FromMinutes(30), Price = 180 },
-        new Treatment() { Id = 2, Klipning = "Dame klip", Duration = TimeSpan.FromMinutes(60), Price = 250 },
-        new Treatment() { Id = 3, Klipning = "Børn klip (under 12 år)", Duration = TimeSpan.FromMinutes(30), Price = 170 },
-        new Treatment() { Id = 4, Klipning = "Herrer klip (pensionist)", Duration = TimeSpan.FromMinutes(30), Price = 170 },
-        new Treatment() { Id = 5, Klipning = "Dame klip (pensionist)", Duration = TimeSpan.FromMinutes(30), Price = 230 },
+        new Treatment() { Id = 1, TreatmentTitle = "Klip: Herrer", Duration = TimeSpan.FromMinutes(30), Price = 180 },
+        new Treatment() { Id = 2, TreatmentTitle = "Klip: Dame", Duration = TimeSpan.FromMinutes(60), Price = 250 },
+        new Treatment() { Id = 3, TreatmentTitle = "Klip: Børn (under 12 år)", Duration = TimeSpan.FromMinutes(30), Price = 170 },
+        new Treatment() { Id = 4, TreatmentTitle = "Klip: Herrer (pensionist)", Duration = TimeSpan.FromMinutes(30), Price = 170 },
+        new Treatment() { Id = 5, TreatmentTitle = "Klip: Dame (pensionist)", Duration = TimeSpan.FromMinutes(30), Price = 230 },
 
-        new Treatment() { Id = 6, Permanent = "Kort fra", Duration = TimeSpan.FromMinutes(60), Price = 550 },
-        new Treatment() { Id = 7, Permanent = "Mellem", Duration = TimeSpan.FromMinutes(90), Price = 750 },
-        new Treatment() { Id = 8, Permanent = "Langt fra", Duration = TimeSpan.FromMinutes(120), Price = 950 },
+        new Treatment() { Id = 6, TreatmentTitle = "Permanent: Kort fra", Duration = TimeSpan.FromMinutes(60), Price = 550 },
+        new Treatment() { Id = 7, TreatmentTitle = "Permanent: Mellem", Duration = TimeSpan.FromMinutes(90), Price = 750 },
+        new Treatment() { Id = 8, TreatmentTitle = "Permanent: Langt fra", Duration = TimeSpan.FromMinutes(120), Price = 950 },
 
-        new Treatment() { Id = 9, Striber = "Kort fra", Duration = TimeSpan.FromMinutes(60), Price = 550 },
-        new Treatment() { Id = 10, Striber = "Mellem", Duration = TimeSpan.FromMinutes(60), Price = 700 },
-        new Treatment() { Id = 11, Striber = "Langt fra", Duration = TimeSpan.FromMinutes(90), Price = 850 },
-        new Treatment() { Id = 12, Striber = "Hætte striber", Duration = TimeSpan.FromMinutes(45), Price = 400 },
+        new Treatment() { Id = 9, TreatmentTitle = "Striber: Kort fra", Duration = TimeSpan.FromMinutes(60), Price = 550 },
+        new Treatment() { Id = 10, TreatmentTitle = "Striber: Mellem", Duration = TimeSpan.FromMinutes(60), Price = 700 },
+        new Treatment() { Id = 11, TreatmentTitle = "Striber: Langt fra", Duration = TimeSpan.FromMinutes(90), Price = 850 },
+        new Treatment() { Id = 12, TreatmentTitle = "Striber: Hætte striber", Duration = TimeSpan.FromMinutes(45), Price = 400 },
 
-        new Treatment() { Id = 13, Helfarvning = "Kort", Duration = TimeSpan.FromMinutes(45), Price = 450 },
-        new Treatment() { Id = 14, Helfarvning = "Mellem", Duration = TimeSpan.FromMinutes(60), Price = 600 },
-        new Treatment() { Id = 15, Helfarvning = "Langt", Duration = TimeSpan.FromMinutes(80), Price = 700 },
-        new Treatment() { Id = 16, Helfarvning = "toning- bund 2-3 cm", Duration = TimeSpan.FromMinutes(45), Price = 350}
+        new Treatment() { Id = 13, TreatmentTitle = "Helfarvning: Kort", Duration = TimeSpan.FromMinutes(45), Price = 450 },
+        new Treatment() { Id = 14, TreatmentTitle = "Helfarvning: Mellem", Duration = TimeSpan.FromMinutes(60), Price = 600 },
+        new Treatment() { Id = 15, TreatmentTitle = "Helfarvning: Langt", Duration = TimeSpan.FromMinutes(80), Price = 700 },
+        new Treatment() { Id = 16, TreatmentTitle = "toning- bund 2-3 cm", Duration = TimeSpan.FromMinutes(45), Price = 350}
     };
 
     public static List<Booking> Bookings { get; } = new()
--- CutNgo/CutNgoDomain/Models/Treatment.cs
+++ CutNgo/CutNgoDomain/Models/Treatment.cs
@@ -3,10 +3,7 @@
 public class Treatment
 {
     public int Id { get; set; }
-    public string Klipning { get; set; } = string.Empty;
-    public string Permanent { get; set; } = string.Empty;
-    public string Striber { get; set; } = string.Empty;
-    public string Helfarvning { get; set; } = string.Empty;
+    public string TreatmentTitle { get; set; } = string.Empty;
     public TimeSpan Duration { get; set; }
     public decimal Price { get; set; }
 }
